e95842c20f01f39ef85be4762394e009a18f7d08
Ze Gan <ganze718@gmail.com>
2023-03-08 11:23:44 +0800

    update
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- memory/app_db_gen.py
+++ memory/app_db_gen.py
@@ -1,12 +1,19 @@
 #!/bin/python3
 
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
 import random
 import redis
 import collections
 import ipaddress
 import uuid
 import struct
-
+import binascii
+import math
+import zlib
+import app_db_pb2
+# import mysql.connector
 
 VNET_COUNT = 1024
 ENI_COUNT = 64
@@ -26,6 +33,8 @@ CPS = 1.5 * 1000 * 1000
 FIXED_ENI = "F4939FEFC47E"
 FIXED_VNI = 2000
 FIXED_MAC = "F9:22:83:99:22:A2"
+FIXED_PREFIES_PER_TAG = 8000
+FIXED_PREFIXES_PER_RULE = 8000
 
 # Dev settings
 
@@ -39,36 +48,255 @@ FIXED_MAC = "F9:22:83:99:22:A2"
 # OPTIONS
 INCLUDE_ACL = True
 ACL_INCLUDE_PREFIX = True
-SAVE_PREFIX_AS_BINARY = False
+# SAVE_PREFIX_AS_BINARY = True
+TO_BINARY = True
+KEY_AS_BINARY = False
+HASH_KEY_AS_BINARY = False
+MOVE_HASH_TO_ONE = True
 USE_TAG = True
-TABLE_NAME_AS_BINARY = False
-TABLE_SET = {}
+IS_IPV4 = False
+TABLE_SET = collections.defaultdict(dict)
+USE_HASH_TABLE = True
+GZIP_VALUE = False
+PROTOBUF_VALUE = True
+KEY_ONLY = False
+RANDOM_GEN = True
+TABLE_SET_REVERSE = {}
+DUMP_TO_MYSQL = False
+
+SAMPLE_KEYS = []
+
+
+# class MySQL(object):
+#     def __init__(self):
+#         self.con = mysql.connector.connect(database="app_db")
+#         self.cursor = self.con.cursor()
+#         self.available_table = set()
+
+#     def create_table(self, table_name, attribute_name):
+#         if table_name in self.available_table:
+#             return
+#         self.cursor = self.con.cursor()
+#         self.cursor.execute("SHOW TABLES LIKE '{}';".format(table_name))
+#         if self.cursor.fetchone() is not None:
+#             self.available_table.add(table_name)
+#             return
+#         create_table_format = b"CREATE TABLE %b (entry_key VARCHAR(255), %b PRIMARY KEY (entry_key));"
+#         attribute_string = b""
+#         for attr in attribute_name:
+#             attribute_string += attr + b" MEDIUMBLOB, "
+#         create_table = create_table_format % (table_name.encode("utf-8"), attribute_string)
+#         print(create_table)
+#         self.cursor.execute(create_table)
+
+#     def insert_entry(self, table_name, entry_key, attribute_value):
+#         if table_name not in self.available_table:
+#             self.create_table(table_name, attribute_value.keys())
+#         insert_format = [b"INSERT INTO ","", b" (entry_key, ", "", b") VALUES (%s, ", b"", b") ON DUPLICATE KEY UPDATE;"]
+#         attribute_names = b", ".join(attribute_value.keys())
+#         value_string = b", ".join([b"%s"] * len(attribute_value))
+#         insert_format[1] = table_name.encode("utf-8")
+#         insert_format[3] = attribute_names
+#         insert_format[5] = value_string
+#         insert_format = b"".join(insert_format)
+#         print(insert_format)
+#         self.cursor.execute(insert_format, [entry_key] + list(attribute_value.values()))
+
+#     def commit(self):
+#         self.con.commit()
+
+#     def close(self):
+#         self.cursor.close()
+#         self.con.close()
+
+
+# sql = MySQL()
+
+
+class Protocol(object):
+    def __init__(self, protocol) -> None:
+        self.protocol = protocol
+
+    def __str__(self):
+        return str(self.protocol)
+
+    def to_binary(self):
+        return struct.pack("B", self.protocol)
+
+    def to_pb(self):
+        return self.protocol
+
+class Port(object):
+    def __init__(self, port) -> None:
+        self.port = port
+
+    def __str__(self):
+        return str(self.port)
+
+    def to_binary(self):
+        return struct.pack("H", self.port)
+
+    def to_pb(self):
+        return self.port
+
+class MAC(object):
+    def __init__(self, mac) -> None:
+        self.mac = mac
+
+    def __str__(self):
+        return str(self.mac)
+
+    def to_binary(self):
+        return binascii.unhexlify(self.mac.replace(":", ""))
+
+    def to_pb(self):
+        return self.to_binary()
+
+
+def value_to_pb(value):
+    if False:
+        pass
+    # if isinstance(value, (ipaddress.IPv4Network, ipaddress.IPv6Network)):
+    #     return value.network_address.packed + struct.pack("B", value.prefixlen)
+    elif isinstance(value, ipaddress.IPv4Network):
+        prefix = app_db_pb2.IpPrefix()
+        prefix.ip.ipv4 = value.network_address.packed
+        prefix.prefix_len = value.prefixlen
+        return prefix
+    elif isinstance(value, ipaddress.IPv6Network):
+        prefix = app_db_pb2.IpPrefix()
+        prefix.ip.ipv6 = value.network_address.packed
+        prefix.prefix_len = value.prefixlen
+        return prefix
+    elif isinstance(value, ipaddress.IPv4Address):
+        ip = app_db_pb2.IpAddress()
+        ip.ipv4 = value.packed
+        return ip
+    elif isinstance(value, ipaddress.IPv6Address):
+        ip = app_db_pb2.IpAddress()
+        ip.ipv6 = value.packed
+        return ip
+    elif isinstance(value, (Protocol, Port, MAC)):
+        return value.to_pb()
+    elif isinstance(value, uuid.UUID):
+        return str(value)
+    else:
+        return value
+
+
+def values_to_pb(table, values):
+    if table == "DASH_VNET_TABLE":
+        entry = app_db_pb2.Vnet()
+    elif table == "DASH_VNET_MAPPING_TABLE":
+        entry = app_db_pb2.VnetMapping()
+    elif table == "DASH_PREFIX_TAG_TABLE":
+        entry = app_db_pb2.PrefixTag()
+    elif table == "DASH_ENI_TABLE":
+        entry = app_db_pb2.Eni()
+    elif table == "DASH_ACL_RULE_TABLE":
+        entry = app_db_pb2.AclRule()
+    elif table == "DASH_ROUTE_TABLE":
+        entry = app_db_pb2.Route()
+    elif table == "DASH_ROUTE_RULE_TABLE":
+        entry = app_db_pb2.RouteRule()
+    else:
+        return None
+    for k, v in values.items():
+        if hasattr(v, "__iter__") and not isinstance(v, (str, ipaddress.IPv4Network, ipaddress.IPv6Network)):
+            for i in v:
+                getattr(entry, k).append(value_to_pb(i))
+        else:
+            field = getattr(entry, k)
+            if hasattr(field, "CopyFrom"):
+                field.CopyFrom(value_to_pb(v))
+            else:
+                setattr(entry, k, value_to_pb(v))
+    return entry.SerializeToString()
+
+
+def to_binary(v):
+    if isinstance(v, (ipaddress.IPv4Network, ipaddress.IPv6Network)):
+        return v.network_address.packed + struct.pack("B", v.prefixlen)
+    elif isinstance(v, (ipaddress.IPv4Address, ipaddress.IPv6Address)):
+        return v.packed
+    elif isinstance(v, (Protocol, Port, MAC)):
+        return v.to_binary()
+    elif isinstance(v, bytes):
+        return v
+    else:
+        return str(v).encode("utf-8") + b":"
+
 
 def insert_entry(rp, table, key, values):
+    useful_size = 0
+    if PROTOBUF_VALUE:
+        field_name = "0"
+        field_value = values_to_pb(table, values)
     for k, v in values.items():
         if isinstance(v, list):
-            if v and isinstance(v[0], ipaddress.IPv6Network):
-                # values[k] = ",".join([x.exploded for x in v])
-                values[k] = b""
-                if SAVE_PREFIX_AS_BINARY:
-                    for x in v:
-                            values[k] += (x.network_address.packed + struct.pack("B", x.prefixlen))
-                else:
-                    values[k] = ",".join([x.exploded for x in v])
+            if TO_BINARY:
+                values[k] = b"".join([to_binary(x) for x in v])
             else:
                 values[k] = ",".join([str(x) for x in v])
         else:
-            values[k] = str(v)
-    if hasattr(key, "__iter__") and not isinstance(key, str):
-        key = ":".join([str(x) for x in key])
-    if TABLE_NAME_AS_BINARY:
+            if TO_BINARY:
+                values[k] = to_binary(v)
+            else:
+                values[k] = str(v)
+    if KEY_AS_BINARY:
         if table not in TABLE_SET:
-            TABLE_SET[table] = len(TABLE_SET)
-        table = struct.pack("B", TABLE_SET[table])
-        key = table + b":" + str(key).encode("utf-8")
+            TABLE_SET[table]["table_id"] = len(TABLE_SET)
+            TABLE_SET_REVERSE[len(TABLE_SET_REVERSE)] = table
+        table_id = struct.pack("B", TABLE_SET[table]["table_id"])
+        if hasattr(key, "__iter__") and not isinstance(key, str):
+            # key = b"".join([to_binary(x) for x in key])
+            bkey = b""
+            for i in range(len(key)):
+                if isinstance(key[i], str):
+                    bkey += key[i].encode("utf-8")
+                    if i != len(key) - 1:
+                        bkey += b":" # separator
+                else:
+                    bkey += to_binary(key[i])
+            key = bkey
+        key = table_id + b":" + to_binary(key)
     else:
+        if hasattr(key, "__iter__") and not isinstance(key, str):
+            key = ":".join([str(x) for x in key])
         key = table + ":" + str(key)
-    rp.hset(key, mapping=values)
+    if HASH_KEY_AS_BINARY:
+        bvalues = {}
+        hash_key_map = {}
+        for k, v in values.items():
+            if k not in hash_key_map:
+                hash_key_map[k] = len(hash_key_map)
+            bvalues[struct.pack("B", hash_key_map[k])] = v
+        values = bvalues
+    if KEY_ONLY:
+        values = {b"Placeholder": b"Placeholder"}
+        value = b"Placeholder"
+    if GZIP_VALUE:
+        value = zlib.compress(value)
+    if MOVE_HASH_TO_ONE:
+        hash_key = b"\xff" * int(math.ceil(len(values) / 8))
+        hash_value = b"".join(values.values())
+        value = hash_key + hash_value
+        values = {hash_key: hash_value}
+    if PROTOBUF_VALUE:
+        value = field_value
+        values = {field_name: field_value}
+    if USE_HASH_TABLE:
+        rp.hset(key, mapping=values)
+        # if DUMP_TO_MYSQL:
+        #     sql.insert_entry(table, key.replace(table + ":", ""), values)
+        useful_size = len(key)
+        for k, v in values.items():
+            useful_size += len(k) + len(v)
+    else:
+        rp.set(key, value)
+        useful_size = len(key) + len(value)
+        pass
+    return key, useful_size
 
 
 class FieldGenerator(object):
@@ -91,18 +319,30 @@ class FieldGenerator(object):
 
     def gen_vni(self):
         self.vnet_id += 1
-        return self.vni
+        return str(self.vni)
 
-    def gen_prefix(self, key=None, is_ipv4=False):
-        self.prefix_id[key] += 1
+    def gen_prefix(self, key=None, is_ipv4=IS_IPV4):
+        if RANDOM_GEN:
+            if is_ipv4:
+                self.prefix_id[key] = random.randint(0, 2 ** 32 - 1)
+            else:
+                self.prefix_id[key] = random.randint(0, 2 ** 126 - 1)
+        else:
+            self.prefix_id[key] += 1
         if is_ipv4:
             prefix = ipaddress.IPv4Network(self.prefix_id[key], strict=False)
         else:
             prefix = ipaddress.IPv6Network(self.prefix_id[key], strict=False)
         return prefix
 
-    def gen_ip(self, key=None, is_ipv4=False):
-        self.address_id[key] += 1
+    def gen_ip(self, key=None, is_ipv4=IS_IPV4):
+        if RANDOM_GEN:
+            if is_ipv4:
+                self.address_id[key] = random.randint(0, 2 ** 32 - 1)
+            else:
+                self.address_id[key] = random.randint(0, 2 ** 126 - 1)
+        else:
+            self.address_id[key] += 1
         if is_ipv4:
             address = ipaddress.IPv4Address(self.address_id[key])
         else:
@@ -111,7 +351,7 @@ class FieldGenerator(object):
 
     def gen_port(self, key=None):
         self.port_id[key] += 1
-        return self.port_id[key] % 65536
+        return Port(self.port_id[key] % 65536)
 
     def gen_port_range(self, key=None):
         start = self.gen_port(key)
@@ -123,7 +363,7 @@ class FieldGenerator(object):
 
     def gen_protocol(self, key=None):
         self.protocol_id[key] += 1
-        return self.protocol_id[key] % 256
+        return Protocol(self.protocol_id[key] % 256)
 
     def gen_guid(self):
         return uuid.uuid4()
@@ -136,10 +376,10 @@ class FieldGenerator(object):
         return random.randint(1, self.acl_group_id)
 
 
-def gen_list(gen_func, count=10):
-    if count <= 1:
-        count = 1
-    return [gen_func() for i in range(int(count))]
+def gen_list(gen_func, count=10, *args, **kwargs):
+    # if count <= 1:
+    #     count = 1
+    return [gen_func(*args, **kwargs) for i in range(int(count))]
 
 
 def gen_vnets(rp, fg):
@@ -170,7 +410,7 @@ def gen_acl(rp, fg):
         insert_entry(
             rp,
             "DASH_ACL_IN_TABLE",
-            (FIXED_ENI, fg.pick_acl_group_id()),
+            (MAC(FIXED_ENI), fg.pick_acl_group_id()),
             {
                 "acl_group_id": fg.pick_acl_group_id(),
             }
@@ -178,7 +418,7 @@ def gen_acl(rp, fg):
         insert_entry(
             rp,
             "DASH_ACL_OUT_TABLE",
-            (FIXED_ENI, fg.pick_acl_group_id()),
+            (MAC(FIXED_ENI), fg.pick_acl_group_id()),
             {
                 "acl_group_id": fg.pick_acl_group_id(),
             }
@@ -246,8 +486,8 @@ def gen_acl(rp, fg):
                     "dst_addr": prefix[int(len(prefix) / 2):],
                     "src_port": gen_list(fg.gen_port, port_per_rule / 2),
                     "dst_port": gen_list(fg.gen_port, port_per_rule / 2),
-                    "src_tag": 0xffffffff,
-                    "dst_tag": 0xffffffff,
+                    "src_tag": ["0xffffffff"],
+                    "dst_tag": ["0xffffffff"],
                 }
             )
             print("ACL_RULE", i * ACL_RULES_PER_NSG + j, "/", rule_per_eni)
@@ -259,7 +499,7 @@ def gen_route(rp, fg):
         insert_entry(
             rp,
             "DASH_ROUTE_TABLE",
-            (FIXED_ENI, FIXED_VNI, fg.gen_prefix()),
+            (MAC(FIXED_ENI), FIXED_VNI, fg.gen_prefix()),
             {
                 "action_type": "vnet",
                 "vnet": fg.pick_vnet_name(),
@@ -279,7 +519,7 @@ def gen_route(rp, fg):
         insert_entry(
             rp,
             "DASH_ROUTE_RULE_TABLE",
-            (FIXED_ENI, FIXED_VNI, fg.gen_prefix()),
+            (MAC(FIXED_ENI), FIXED_VNI, fg.gen_prefix()),
             {
                 "action_type": "decap",
                 "priority": 2**32-1,
@@ -298,11 +538,11 @@ def gen_map(rp, fg):
         insert_entry(
             rp,
             "DASH_VNET_MAPPING_TABLE",
-            (FIXED_ENI, fg.gen_ip()),
+            (fg.pick_vnet_name(), fg.gen_ip()),
             {
                 "routing_type": "vnet_encap",
                 "underlay_ip": fg.gen_ip(),
-                "mac_address": FIXED_MAC,
+                "mac_address": MAC(FIXED_MAC),
                 "metering_bucket": 0,
                 "use_dst_vni": True,
                 "use_pl_sip_eni": True,
@@ -312,18 +552,149 @@ def gen_map(rp, fg):
         )
         print("DASH_VNET_MAPPING_TABLE", i, "/", CA_PA_MAPPING / ENI_COUNT)
 
+def gen_sample(rp, fg):
+    key = insert_entry(
+        rp,
+        "DASH_VNET_TABLE",
+        fg.gen_vnet_name(),
+        {
+            "vni": fg.gen_vni(),
+            "guid": fg.gen_guid(),
+            "address_space": gen_list(fg.gen_prefix),
+            "peer_list": gen_list(fg.pick_vnet_name),
+        })
+    SAMPLE_KEYS.append(key)
+
+    key = insert_entry(
+        rp,
+        "DASH_VNET_MAPPING_TABLE",
+        (fg.gen_vnet_name(), fg.gen_ip()),
+        {
+            "routing_type": "vnet_encap",
+            "underlay_ip": fg.gen_ip(),
+            "mac_address": MAC(FIXED_MAC),
+            "metering_bucket": 0,
+            "use_dst_vni": True,
+            "use_pl_sip_eni": True,
+            "overlay_sip": fg.gen_ip(),
+            "overlay_dip": fg.gen_ip(),
+        }
+    )
+    SAMPLE_KEYS.append(key)
+
+    for prefix_count in [0, 10, 100, 200, 1000, 2000, 8000]:
+        key = insert_entry(
+            rp,
+            "DASH_PREFIX_TAG_TABLE",
+            ("prefix", prefix_count),
+            {
+                "prefix_list": gen_list(fg.gen_prefix, prefix_count),
+            }
+        )
+        SAMPLE_KEYS.append(key)
+
+    key = insert_entry(
+        rp,
+        "DASH_ENI_TABLE",
+        MAC(FIXED_MAC),
+        {
+            "eni_id": MAC(FIXED_MAC),
+            "mac_address": MAC(FIXED_MAC),
+            "qos": "qos_name",
+            "underlay_ip": fg.gen_ip(),
+            "admin_state": "disabled",
+            "vnet": fg.gen_vnet_name(),
+            "pl_sip_encoding": fg.gen_prefix(),
+            "pl_underlay_sip": fg.gen_ip(),
+        }
+    )
+    SAMPLE_KEYS.append(key)
+
+    for prefix_count in [0, 10, 100, 200, 1000, 2000, 8000]:
+        key = insert_entry(
+            rp,
+            "DASH_ACL_RULE_TABLE",
+            ("group_id", prefix_count),
+            {
+                "priority": 2**32-1,
+                "action": "allow",
+                "terminating": True,
+                "protocol": gen_list(fg.gen_protocol, 255),
+                "src_addr": gen_list(fg.gen_prefix, prefix_count/2),
+                "dst_addr": gen_list(fg.gen_prefix, prefix_count/2),
+                "src_port": gen_list(fg.gen_port, 5),
+                "dst_port": gen_list(fg.gen_port, 5),
+                "src_tag": ["prefix" + str(prefix_count)],
+                "dst_tag": ["prefix" + str(prefix_count)],
+            }
+        )
+        SAMPLE_KEYS.append(key)
+
+    key = insert_entry(
+        rp,
+        "DASH_ROUTE_TABLE",
+        (MAC(FIXED_ENI), FIXED_VNI, fg.gen_prefix()),
+        {
+            "action_type": "vnet",
+            "vnet": fg.gen_vnet_name(),
+            "appliance": 0,
+            "overlay_ip": fg.gen_ip(),
+            "overlay_sip": fg.gen_ip(),
+            "overlay_dip": fg.gen_ip(),
+            "underlay_sip": fg.gen_ip(),
+            "underlay_dip": fg.gen_ip(),
+            "metering_bucket": 0,
+        }
+    )
+    SAMPLE_KEYS.append(key)
+
+    key = insert_entry(
+        rp,
+        "DASH_ROUTE_RULE_TABLE",
+        (MAC(FIXED_ENI), FIXED_VNI, fg.gen_prefix()),
+        {
+            "action_type": "decap",
+            "priority": 2**32-1,
+            "protocol": fg.gen_protocol(),
+            "vnet": fg.pick_vnet_name(),
+            "pa_validation": True,
+            "metering_bucket": 0,
+            "region": 0
+        }
+    )
+    SAMPLE_KEYS.append(key)
 
 fg = FieldGenerator()
+
 r = redis.Redis(host='localhost', port=6379, db=0)
 rp = r.pipeline()
 
-gen_vnets(rp, fg)
-if INCLUDE_ACL:
-    gen_acl(rp, fg)
-gen_route(rp, fg)
-gen_map(rp, fg)
+# gen_vnets(rp, fg)
+# if INCLUDE_ACL:
+#     gen_acl(rp, fg)
+# gen_route(rp, fg)
+# gen_map(rp, fg)
+
+gen_sample(rp, fg)
 
 rp.execute()
 
-if TABLE_NAME_AS_BINARY:
-    print(TABLE_SET)
+def report_memory_usage(r):
+    if KEY_AS_BINARY:
+        print(TABLE_SET)
+    # Total memory
+    print("Used memory human: ", r.info("memory")["used_memory_human"])
+    for key, useful_size in SAMPLE_KEYS:
+        if KEY_AS_BINARY:
+            table_id = struct.unpack("B", key[0:1])[0]
+            table_name = TABLE_SET_REVERSE[table_id]
+            print("Table ", table_name, " Key ", key ," type ", r.object("encoding", key), " entry size ", r.memory_usage(key, 0), " useful size ", useful_size)
+        else:
+            print("Key ", key ," type ", r.object("encoding", key), " entry size ", r.memory_usage(key, 0), " useful size ", useful_size)
+
+
+report_memory_usage(r)
+
+# if DUMP_TO_MYSQL:
+#     sql.commit()
+#     sql.close()
